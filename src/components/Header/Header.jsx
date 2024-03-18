8b6a4d3c62984f195a22c199212ed4e0f363c571
mostafizur rahman <dev.mostafizurrahman@gmail.com>
2024-03-19 05:36:09 +0600

    header added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from "react";
+import "./Header.css"
 
 const Header = () => {
-    return (
-        <div>
-         <h2>This is header</h2>
-        </div>
-    );
+  return (
+    <div className="header">
+      <h2>Ema-Jhon</h2>
+
+      <div>
+        <a href="/home">Home</a>
+        <a href="/order">Order</a>
+        <a href="/about">About</a>
+        <a href="/invertory">Inventory</a>
+        <a href="/contact">Contact</a>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
